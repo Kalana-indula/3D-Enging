c86087e6373992d7655635aee4bbc3006b9984e7
Kalana-indula <kalanasmedia@outlook.com>
2025-01-22 14:48:11 +0530

    2D drawing created

--- src/main/java/DemoViewer.java
+++ src/main/java/DemoViewer.java
@@ -1,42 +1,114 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DemoViewer {
 
-    //Create a triangle list to form tetrahedron
-    private List<Triangle> tris=new ArrayList<>();
-
-    public DemoViewer(){
+    public DemoViewer() {
         //creating a new window
-        JFrame frame=new JFrame();
-        Container pane=frame.getContentPane();
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pane.setLayout(new BorderLayout());
 
         //adding a slider to control horizontal rotation
-        JSlider headingSlider=new JSlider(0,360,180);
-        pane.add(headingSlider,BorderLayout.SOUTH);
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
 
         //slider to control vertical rotation
-        JSlider pitchSlider=new JSlider(SwingConstants.VERTICAL,-90,90,0);
-        pane.add(pitchSlider,BorderLayout.EAST);
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        //creating a triangle collection
+        List<Triangle> tris = new ArrayList<>();
+
+        tris.add(new Triangle(
+                new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(-100, 100, -100),
+                Color.WHITE));
+
+        tris.add(new Triangle(
+                new Vertex(100, 100, 100),
+                new Vertex(-100, -100, 100),
+                new Vertex(100, -100, -100),
+                Color.RED));
+
+        tris.add(new Triangle(
+                new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(100, 100, 100),
+                Color.GREEN));
+
+        tris.add(new Triangle(
+                new Vertex(-100, 100, -100),
+                new Vertex(100, -100, -100),
+                new Vertex(-100, -100, 100),
+                Color.BLUE));
+
 
         //panel to display render results
-        JPanel renderPanel=new JPanel(){
-            public void paintComponent(Graphics g){
-                Graphics2D g2=(Graphics2D) g;
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
                 g2.setColor(Color.BLACK);
-                g2.fillRect(0,0,getWidth(),getHeight());
+                g2.fillRect(0, 0, getWidth(), getHeight());
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+
+                for(Triangle t:tris){
+                    //draw a path in 2D plane
+                    Path2D path=new Path2D.Double();
+                    path.moveTo(t.v1.x,t.v1.y);
+                    path.lineTo(t.v2.x,t.v2.y);
+                    path.lineTo(t.v3.x,t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
-        pane.add(renderPanel,BorderLayout.CENTER);
+        pane.add(renderPanel, BorderLayout.CENTER);
 
-        frame.setSize(400,400);
+        frame.setSize(400, 400);
         frame.setVisible(true);
-
-        //create four triangles
 
     }
 }
+
+//creating vertexes
+class Vertex {
+    //create position of vertex
+    double x;
+    double y;
+    double z;
+
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+//creating a triangle
+class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+
+    //create a triangle as the instance created
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
+
+
+
+
+
